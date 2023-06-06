29d51a1f7bb7daaf4cf138289053b319b3a40c5b
pedrofps1 <pedrofpsantos@ua.pt>
2023-06-06 21:24:24 +0100

    Update sp.sql

--- SQL scripts/sp.sql
+++ SQL scripts/sp.sql
@@ -23,6 +23,7 @@ BEGIN
   DELETE FROM Otica_Pessoa WHERE CC = @CC;
 END
 
+
 CREATE PROCEDURE AgendarConsulta
   @numero_consulta INT,
   @hora TIME,
@@ -36,8 +37,6 @@ BEGIN
   INSERT INTO Otica_Consulta (numero_consulta, hora, preço, data_consulta, recepcionista_cc, doutor_cc, cliente_cc)
   VALUES (@numero_consulta, @hora, @preço, @data_consulta, @recepcionista_cc, @doutor_cc, @cliente_cc);
 END
-
-
 
 
 CREATE PROCEDURE RegistarVendaProduto
@@ -55,8 +54,6 @@ BEGIN
 END
 
 
-
-
 CREATE PROCEDURE RegistarEntregaProduto
   @id INT,
   @data_entrada DATE,
@@ -68,9 +65,6 @@ BEGIN
   INSERT INTO Otica_Stock (id, produto_id, data_entrada, estado, quantidade)
   VALUES (@id, @produto_id, @data_entrada, @estado, @quantidade);
 END
-
-
-
 
 
 CREATE PROCEDURE RegistarPagamentoFatura
@@ -86,3 +80,36 @@ BEGIN
   INSERT INTO Otica_Pagamento (id, data, valor, metodo, comprovativo, cliente_cc, fatura_id)
   VALUES (@id, @data, @valor, @metodo, @comprovativo, @cliente_cc, @fatura_id);
 END
+
+
+DROP PROCEDURE getClientebyCC;
+GO
+CREATE PROCEDURE getClientebyCC
+    @CC INT
+AS
+BEGIN
+    SELECT Otica_Pessoa.CC, Otica_Pessoa.nome, Otica_Pessoa.email, Otica_Pessoa.telemovel, Otica_Pessoa.morada, Otica_Cliente.num_contribuinte 
+	FROM Otica_Pessoa JOIN Otica_Cliente ON Otica_Pessoa.CC = Otica_Cliente.CC
+
+    WHERE Otica_Pessoa.CC = @CC;
+END;
+
+
+CREATE PROCEDURE AtualizarCliente
+    @CC INT,
+    @NovoNome VARCHAR(50),
+    @NovoEmail VARCHAR(60),
+	@NovoMorada VARCHAR(200),
+	@NovoTelemovel INT,
+	@NovoNum_Contribuinte INT
+AS
+BEGIN
+    UPDATE Otica_Pessoa
+    SET nome = @NovoNome, email = @NovoEmail, morada = @NovoMorada , telemovel = @NovoTelemovel
+    WHERE CC = @CC
+	UPDATE Otica_Cliente
+	SET num_contribuinte = @NovoNum_Contribuinte 
+	WHERE CC = @CC
+END;
+
+
