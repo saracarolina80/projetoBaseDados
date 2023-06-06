a5824277377052776f6482e2834db7d498459ae9
pedrofps1 <pedrofpsantos@ua.pt>
2023-06-06 22:00:10 +0100

    agora sim

--- SQL scripts/views.sql
+++ SQL scripts/views.sql
@@ -1,18 +1,19 @@
 
-
+GO
 CREATE VIEW ConsultasPorCliente AS
 SELECT Otica_Cliente.CC, Otica_Cliente.nome, COUNT(Otica_Consulta.numero_consulta) AS total_consultas
 FROM Otica_Cliente
 JOIN Otica_Consulta ON Otica_Cliente.CC = Otica_Consulta.cliente_cc
 GROUP BY Otica_Cliente.CC, Otica_Cliente.nome;
 
+GO
 CREATE VIEW DoutoresConsultas AS
 SELECT Otica_Doutor.CC, Otica_Doutor.num_SNS, COUNT(Otica_Consulta.numero_consulta) AS total_consultas
 FROM Otica_Doutor
 JOIN Otica_Consulta ON Otica_Doutor.CC = Otica_Consulta.doutor_cc
 GROUP BY Otica_Doutor.CC, Otica_Doutor.num_SNS;
 
-
+GO
 CREATE VIEW StockProdutos AS
 SELECT Otica_Produto.id, Otica_Produto.nome, Otica_Stock.quantidade
 FROM Otica_Produto
